1815ae390d05fb31a9d751710a8f61b18cf68837
Andrew Krawchyk <akrawchyk@gmail.com>
2014-11-23 21:18:51 -0500

    update image with text input toggle

--- src/js/controllers/block_controller.js
+++ src/js/controllers/block_controller.js
@@ -9,6 +9,24 @@
     actions: {
       editBlock: function() {
         this.set('isEditing', true);
+      },
+      toggleEdit: function() {
+        var editing = this.get('isEditing');
+
+        this.set('isEditing', !editing);
+      },
+
+      updateImage: function() {
+        var newImage = this.get('newImage');
+
+        if (Ember.isEmpty(newImage)) {
+          return;
+        }
+
+        var block = this.get('model');
+        block.set('image', newImage);
+        block.save();
+        console.log('updated image with ' + newImage);
       }
     },
 
